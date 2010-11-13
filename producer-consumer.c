393d5792a653a95eb890e912543e9dde6b537380
Tiago Rodrigo Kepe <tiagokepe@gmail.com>
2010-11-13 19:49:40 -0200

    Enviando e ecoando as mensagens

--- producer-consumer.c
+++ producer-consumer.c
@@ -1,5 +1,11 @@
 #include "producer-consumer.h"
 
+static void clear_buff(char **buff) {
+    int i;
+    for(i=0; i < MAX_LINE; i++)
+        (*buff)[i] = '\0';
+}
+
 prod_cons_t *cria_prod_cons(void) {
     prod_cons_t *prod_cons = (prod_cons_t *) malloc(sizeof(prod_cons_t));
 
@@ -22,7 +28,7 @@ void *insert_buffer(prod_cons_t *prod_cons) {
         char *buffer = (char *)malloc(MAX_LINE*sizeof(char));
         /* produz novo item */
         scanf (" %[^\n]", buffer);
-                printf("Inserindo\n");
+        printf("Entrou - produtor = %s\n", buffer);
         sem_wait(&prod_cons->empty);
         pthread_mutex_lock(&prod_cons->mutex);
 
@@ -34,6 +40,7 @@ void *insert_buffer(prod_cons_t *prod_cons) {
         pthread_mutex_unlock(&prod_cons->mutex);
         sem_post(&prod_cons->full);
 
+        printf("Saiu - produtor\n");
     } while(1);
 
     pthread_exit((void*)0);
@@ -42,7 +49,7 @@ void *insert_buffer(prod_cons_t *prod_cons) {
 /* Consumidor retira do buffer */
 void *remove_buffer(prod_cons_t *prod_cons) {
     char *buff_send;
-    char buff_rec[MAX_LINE];
+    char *buff_rec = (char *)malloc(MAX_LINE*sizeof(char));
     ssize_t res_send;
     unsigned int fromlen;
     struct sockaddr_in from;
@@ -55,19 +62,21 @@ void *remove_buffer(prod_cons_t *prod_cons) {
     fromlen = sizeof(struct sockaddr_in);
 
     /* enviar bastão */
-    if (prod_cons->is_initial == 'B') {
+    if (prod_cons->is_initial == 'B')
         sendto(client->descriptor, MSG_BASTAO, strlen(MSG_BASTAO), 0, (struct sockaddr *)&(client->sock_addr), sizeof(struct sockaddr_in));
-    }
+
     do {
-        printf("Entrou no DO\n");
+//        printf("PROD - DO\n");
+        clear_buff(&buff_rec);
         res_send = recvfrom(server->descriptor, buff_rec, MAX_LINE, 0, (struct sockaddr *)&from, &fromlen);
         printf("Recebeu msg = %s\n", buff_rec);
-        if (res_send < 0) { printf("RRRRRRRRRRRRR\n"); error("recvfrom"); }
+        if (res_send < 0) error("recvfrom");
 
+        /* verifica se recebeu o bastão */
         if ( !strcmp(buff_rec, MSG_BASTAO) ) {
-            printf("BASTAO\n");
             //Fazer send_message()
 
+            printf("Entrou - Consumidor\n");
             sem_wait(&prod_cons->full);
             pthread_mutex_lock(&prod_cons->mutex);
 
@@ -76,12 +85,19 @@ void *remove_buffer(prod_cons_t *prod_cons) {
             printf("Rettirou = %s\n", buff_send);
             prod_cons->buffer[prod_cons->cons_pos] = NULL;
 
+            sendto(client->descriptor, buff_send, strlen(buff_send), 0, (struct sockaddr *)&(client->sock_addr), sizeof(struct sockaddr_in));
+
             prod_cons->cons_pos = (prod_cons->cons_pos + 1) % MAX_BUFF_SIZE;
             prod_cons->buff_filled--;
 
             pthread_mutex_unlock(&prod_cons->mutex);
             sem_post(&prod_cons->empty);
+            printf("Saiu - Consumidor\n");
         }
+        else /* ecoa mensagem */
+            /* talvez tenha que sincronizar para ecoar na tela */
+            fprintf(stdout,"%s\n", buff_rec);
+
     } while(1);
 
 
